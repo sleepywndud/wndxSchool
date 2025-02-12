418a636b54fc143a8b852a59265ae202475af4ca
wnd <w2n2d2@gmail.com>
2025-02-12 15:16:28 +1300

    Update keypad.py

--- keypad.py
+++ keypad.py
@@ -1,43 +1,29 @@
-import os
-import time
-
-def countdown():
-    time.sleep(5)
-
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-#######################################
-
 KEYPAD = {
-    'A': '2', 
-    'B': '2', 
-    'C': '2', 
-    'D': '3', 
-    'E': '3',
-    'F': '3', 
-    'G': '4', 
-    'H': '4', 
-    'I': '4', 
-    'J': '5',
-    'K': '5', 
-    'L': '5', 
-    'M': '6', 
-    'N': '6', 
-    'O': '6',
-    'P': '7', 
-    'Q': '7', 
-    'R': '7', 
-    'S': '7', 
-    'T': '8',
-    'U': '8', 
-    'V': '8', 
-    'W': '9', 
-    'X': '9', 
-    'Y': '9',
-    'Z': '9',
+    'A': '2', 'B': '2', 'C': '2',
+    
+    'D': '3', 'E': '3', 'F': '3',
+    
+    'G': '4', 'H': '4', 'I': '4',
+    
+    'J': '5', 'K': '5', 'L': '5',
+    
+    'M': '6', 'N': '6', 'O': '6',
+    
+    'P': '7', 'Q': '7', 'R': '7', 'S': '7',
+    
+    'T': '8', 'U': '8', 'V': '8',
+    
+    'W': '9', 'X': '9', 'Y': '9', 'Z': '9'
 }
 
-# inp = str(input("Enter word: ")).upper()
+inp = input("Enter word: ").upper()
 
-print(KEYPAD['A'])
+numeric = ""
+
+for hi in inp:
+    if hi in KEYPAD:
+        numeric = numeric + KEYPAD[hi]
+    else:
+        numeric = numeric + hi
+
+print(f"{numeric}\n\n\n")
