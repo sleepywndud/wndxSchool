b03379b21079a14873a55f55e507cf3f51122e1e
wnd <w2n2d2@gmail.com>
2025-02-18 13:57:02 +1300

    Update PythonColorPrint.py

--- useless/PythonColorPrint.py
+++ useless/PythonColorPrint.py
@@ -11,12 +11,4 @@ print('\033[95m') # PURPLE
 print('\033[97m') # WHITE
 print('\033[37m') # LIGHT GRAY
 print('\033[90m') # GRAY
-print('\033[30m') # BLACK
-
-
-print('\033[31m') # LIGHT RED
-print('\033[33m') # ORANGE
-print('\033[32m') # LIGHT GREEN
-print('\033[36m') # LIGHT CYAN
-print('\033[34m') # LIGHT BLUE
-print('\033[35m') # LIGHT PURPLE
+print('\033[30m') # BLACK (I THINK)
