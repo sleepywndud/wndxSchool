63a5aa114a017a60a8574bd08d5ab25f9681317e
wnd <w2n2d2@gmail.com>
2025-03-07 09:44:56 +1300

    Update favouriteflavours.py

--- GROK/favouriteflavours.py
+++ GROK/favouriteflavours.py
@@ -6,11 +6,14 @@ top_20_flavours = [
     "Coffee", "Peanut Butter", "Raspberry Ripple", "Green Tea",
     "Passionfruit"
 ]
+
 icecream = str(input("What is your favourite ice cream? ")).title()
 ic_pos = top_20_flavours.index(f"{icecream}")
 counter = 0
 
 if icecream in top_20_flavours:
     print(f"{icecream} is at position {ic_pos + 1} in the top 20 list.")
+else:
+    print("Your favourite flavour is too unique for this list!")
 
 print(f"Top 20 ice cream flavours are: {", ".join(top_20_flavours)}")
