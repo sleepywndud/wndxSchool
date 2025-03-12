5fd00c09e04482aa6abf57422cb772674adcaacd
wnd <w2n2d2@gmail.com>
2025-03-12 15:15:16 +1300

    Update passwordchecker.py

--- GROK/passwordchecker.py
+++ GROK/passwordchecker.py
@@ -1,10 +1,13 @@
+import os 
+os.system('clear')
+
 minimum = 12
 password = input("Password to check: ")
 
 if 'password' in password:
-    print("You shouldn't use a password in a password!")
-    password = "90penguinRacers"
-    print(f"Here's a suggestion for a passphrase: {password}")
+    print("You shouldn't use 'password' in a password!")
+    newpassword = password.replace("password", "my90penguinRacers")
+    print(f"Here's a suggestion for a passphrase: {newpassword}")
 
 elif len(password) < minimum:
     print("Too short!")
