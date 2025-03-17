97892279a9329fde67b05eca93d9a08767245262
wnd <22222w@duck.com>
2025-03-18 10:36:02 +1300

    Create Q10.py

---
+++ NCEA-Auto/Q10.py
@@ -0,0 +1,24 @@
+import os
+
+if os.system == "nt":
+    os.system("cls")
+else:
+    os.system("clear")
+
+#########################
+
+numbers = []
+userinput = input("Enter a number: ")
+
+while userinput != "quit":
+    integerchecker = int(userinput)
+
+    if integerchecker % 2 == 1: # ODD CONDITION
+        numbers.append(userinput)
+        userinput = input("Enter a number: ")
+
+    elif integerchecker % 2 == 0: # EVEN CONDITION
+        userinput = input("Enter a number: ")
+
+for i in numbers:
+    print(i)
