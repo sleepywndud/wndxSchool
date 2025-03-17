444527028633d1ddaec8d430462feb880dfbe155
wnd <22222w@duck.com>
2025-03-18 10:47:13 +1300

    Create Q3.py

---
+++ NCEA-Auto/Q3.py
@@ -0,0 +1,15 @@
+import os
+
+if os.system == "nt":
+    os.system("cls")
+else:
+    os.system("clear")
+
+#########################
+
+age = int(input("Enter your age: "))
+
+if age != 1:
+    print("You are {age} years old.")
+elif age == 1:
+    print(f"You are {age} year old.")
