da04d539cbfd5e3c0bfc044c3f5200a7e8c77a81
wnd <22222w@duck.com>
2025-03-21 18:31:24 +1300

    Update Q4.py

--- NCEA-Auto/NCEA-Style Questions/Q4.py
+++ NCEA-Auto/NCEA-Style Questions/Q4.py
@@ -12,16 +12,21 @@ maximum = 100
 nerds = 0
 one = 1
 
+grades = []
+
 while True:
     user_input = input("Enter a score, or type 'done' to exit: ")
-    if user_input.upper() == 'DONE': # BREAKS IF INPUT IS 'DONE'
+    if user_input.upper() == 'DONE': # BREAK IF INPUT == 'DONE'
         break
     try:
-        grade = int(user_input) # INTS THE USER INPUT
-        if minimum <= grade <= maximum: # CHECKING TO SEE IF IT MEETS NERDS
-            nerds += one
-    except ValueError: # BLOCKS OTHER DATA TYPES (STR, COMPLEX)
+        grade = int(user_input)  # FLOATS TO INTEGER
+        grades.append(grade)
+    except ValueError: # BLOCKS OTHER VARIABLE TYPES (STR, COMPLEX)
         print("Invalid score!") 
+
+for grade in grades:
+    if minimum <= grade <= maximum:  # FILTER NETRDS
+        nerds += one
 
 if nerds == one:
     print(f"This class has {nerds} smart student!")
