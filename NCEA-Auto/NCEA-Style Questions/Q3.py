487e2d94c96ec8dd30ec0fe17115e7c79360e75f
wnd <22222w@duck.com>
2025-03-21 17:54:40 +1300

    Create Q3.py

---
+++ NCEA-Auto/NCEA-Style Questions/Q3.py
@@ -0,0 +1,30 @@
+import os
+
+if os.system == "nt":
+    os.system("cls")
+else:
+    os.system("clear")
+
+#########################
+
+init = 200
+remaining = [init]
+bankrupt = 0
+
+while True:
+    try:
+        spending = int(input("Enter the amount spent: "))
+        if spending == bankrupt:
+            break
+        else:
+            init -= spending
+            remaining.append(init)
+            if init <= bankrupt:
+                break
+
+    except ValueError:
+        print("That is not a valid transaction.")
+
+print("My bank balances have been:")
+for w in remaining:
+    print(w)
