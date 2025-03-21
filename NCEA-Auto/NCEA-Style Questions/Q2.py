c6a8779573c3472646e773fbef64ea47f5716915
wnd <22222w@duck.com>
2025-03-21 14:04:06 +1300

    Update Q2.py

--- NCEA-Auto/NCEA-Style Questions/Q2.py
+++ NCEA-Auto/NCEA-Style Questions/Q2.py
@@ -18,22 +18,22 @@ counter = 0
 while True:
     try:
         userinput = float(input("Enter the temperature: "))
-        if userinput == byebye:
+        if userinput == byebye: # IMMEDIATELY QUITS IF -1 (BYEBYE) ENTERED
             break
         else:
             temperatures.append(userinput)
 
-    except ValueError:
+    except ValueError: # RESTRICTS ANY OTHER INPUT THAN INT/FLOATS
         print("Invalid temperature.")
 
-for userinput in range(len(temperatures)):
+for userinput in range(len(temperatures)): # INDEXING THROUGH LIST AND PRINTING OUTPUT
     if temperatures[counter] <= cold:
         print("too cold")
         counter += 1
     elif temperatures[counter] >= hot:
         print("too hot")
         counter += 1
-    elif temperatures[counter] >= rightmin and temperatures[counter] <= rightmax:
+    elif temperatures[counter] >= rightmin and temperatures[counter] <= rightmax: # BETWEEN THE 'JUST RIGHT' TEMPERATURE CONDITION
         print("just right")
         counter += 1
 
