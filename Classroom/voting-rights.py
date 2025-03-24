1ffec6aac9786f5b8e2f0f429a3286babb7c932a
wnd <22222w@duck.com>
2025-03-24 13:22:36 +1300

    Create voting-rights.py

---
+++ Classroom/voting-rights.py
@@ -0,0 +1,45 @@
+'''PROGRAM TO DETERMINE WHETHER IF A PERSON CAN VOTE OR NOT.'''
+
+import os
+if os.name == "nt":
+    os.system("cls")
+else:
+    os.system("clear")
+
+#########################
+
+resident = [] # LIST TO HOLD NAMES IF RESIDENCY IS TRUE.
+
+while True:
+    try:
+        name = str(input("Enter your name: ")).strip().title()
+        if len(name) == 0:
+            print("Name cannot be empty. Please enter a valid name.")
+            continue
+
+        age = int(input("Enter your age: "))
+        if age < 0:
+            print("Age cannot be negative. Please enter a valid age.")
+            continue
+
+        resd = str(input("Are you currently a NZ resident? ('True' for yes, or 'False' for no): ")).upper()
+        if resd == 'TRUE' or resd == 'FALSE': # BLOCKS ANY OTHER VARIABLE TYPED INPUT.
+            break
+        else:
+            print("Please enter a valid input.")
+
+    except ValueError: # BLOCKS ANY OTHER VARIABLE TYPED INPUT.
+        print("Please enter a valid input.")
+
+
+if resd == 'TRUE': # ADDS NAME TO THE RESIDENT LIST IF RESIDENT.
+    resident.append(name)
+
+else: # DOES NOTHING IF NOTHING ENTERED. 
+    pass
+
+if age >= 17 and name in resident: # CHECK IF PERSON MEETS THE CONDITIONS TO VOTE. 
+    print("You can vote.")
+
+else:
+    print("You cannot vote.")
