6ff1971e1ba88f68d5c4e5634b7ea6c204a88e0f
wnd <22222w@duck.com>
2025-04-06 18:25:28 +1200

    Update mathquestions.py

--- Classroom/Practice for 92004/mathquestions.py
+++ Classroom/Practice for 92004/mathquestions.py
@@ -1,9 +1,12 @@
 '''PREPARATION PROGRAM FOR THE AS92004 SOFTWARE EXAM'''
 
+# NCEA AUTO does not support os import -- remove before submitting
 import os
 os.system('clear')
 
 # list to hold all questions, options using nested lists, and the answer using dictionaries
+# \n[033 stuffs are coloring the text, which increases visibility
+# spaces in "options" are to make differentiating (between Q, and Option) easier
 questions = [
     {
         "question": "\n\033[95m1 + 1 = \033[97m",
@@ -37,20 +40,26 @@ questions = [
     },
 ]
 
+# loops the whole thing in case the user wants to play again (see startornot userinput further below)
 while True:
     try:
+        # asks the user if they want to play the quiz
         startornot = str(input("Welcome to basic maths! Would you like to start the quiz? (Y/N): ")).strip().lower()
-        if startornot == "y":
-            score = 0
-            for question in questions:
+        if startornot == "y": # starting quiz
+            score = 0 # initializing score
+            for question in questions: 
+                # printing questions and options
                 print(question["question"])
                 for option in question["options"]:
                     print(option)
                 
+                # loop to check if input is valid, and correct
                 while True: 
                     try:
                         answer = str(input("\nType your answer: ")).strip().lower()
+                        # checking if answer is valid (a,b,c,c)
                         if answer in ['a', 'b', 'c', 'd']:
+                            # checking if answer is correct
                             if answer == question["answer"]:
                                 print("\033[92mCorrect!\033[97m")
                                 score += 1
@@ -59,27 +68,35 @@ while True:
                                 print("\033[91mIncorrect!\033[97m")
                                 break
                         else:
+                            # blocks any other inputs that are not a,b,c,d
                             print("Your answer is invalid, please try again.")
 
-                    except ValueError:
+                    except ValueError: # blocks any other inputs that are not strings
                         print("Invalid Input. Please try again.")
 
+            # score printing
             print(f"Your score is: {score}/6!")
+            # asks if user wants to redo the test
             startornot = str(input("Want to redo the test? (Y/N) ")).strip().lower()
             if startornot == "y":
+                # passing since the whole thing will loop again if not broke
                 pass
             elif startornot == "n":
+                # breaks if user input is n
                 break
             else:
+                # blocks any other inputs
                 print("Please enter a vaild input.")
 
+        # breaks straight away if user does not want to play (in starting question)
         elif startornot == "n":
             print("Bye Bozo")
             break
         
+        # asks again if input is invalid in starting question
         else:
             print("Invalid input, please try again.")
-
+    
+    # blocks any other variable (input) types that are not strings
     except ValueError:
-        print("Bye bozo")
-        break
+        print("Invald input, please try again.")
