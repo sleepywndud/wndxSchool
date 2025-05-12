6dc6ad686ba31f9b22bd29d90a04169e694924ca
wnd <22222w@duck.com>
2025-05-13 11:26:09 +1200

    .

--- SQL/Rugby Task/rugby.py
+++ SQL/Rugby Task/rugby.py
@@ -6,6 +6,11 @@ os.system("CLEAR")
 db = sqlite3.connect("Rugby.db")
 cr = db.cursor()
 
-all = cr.execute("SELECT * FROM player;")
+all = cr.execute("""
+SELECT first_name, last_name, debut_year
+                 FROM player
+                 WHERE points_scored = (SELECT MAX(points_scored))  
+""").fetchall()
+
 for i in all:
     print(i)
