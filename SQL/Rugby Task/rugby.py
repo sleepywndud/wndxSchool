ce8732cd460898985353b2430579c546d4fc8367
wnd <22222w@duck.com>
2025-05-21 14:31:05 +1200

    Update rugby.py

--- SQL/Rugby Task/rugby.py
+++ SQL/Rugby Task/rugby.py
@@ -1,60 +1,59 @@
 """Program connecting SQLite (rugby) database and run queries (functions)."""
 
-if __name__ == "__main__":
-    import sqlite3
-    import tabulate
-    import os
+import sqlite3
+import tabulate
+import os
 
-    os.system("CLEAR")
+os.system("CLEAR")
 
-    db = sqlite3.connect("Rugby.db")
-    cr = db.cursor()
+db = sqlite3.connect("Rugby.db")
+cr = db.cursor()
 
-    def all_players():
-        all = cr.execute("""
-            SELECT * 
-            FROM player
-            ;
-        """).fetchall()
+def all_players():
+    all = cr.execute("""
+        SELECT * 
+        FROM player
+        ;
+    """).fetchall()
 
-        for client in all:
-            print(client)
-        db.close()
+    for client in all:
+        print(client)
+    db.close()
 
-    def first_last_name():
-        all = cr.execute("""
-            SELECT first_name, last_name
-            FROM player
-            WHERE position = "Fly-half"
-            ;
-        """).fetchall()
+def first_last_name():
+    all = cr.execute("""
+        SELECT first_name, last_name
+        FROM player
+        WHERE position = "Fly-half"
+        ;
+    """).fetchall()
 
-        for client in all:
-            print(client[0], client[1])
-        db.close()
+    for client in all:
+        print(client[0], client[1])
+    db.close()
 
-    def most_point():
-        all = cr.execute("""
-            SELECT first_name, last_name, debut_year
-            FROM player
-            WHERE points_scored = (SELECT MAX(points_scored) FROM player)
-            ;
-        """).fetchall()
+def most_point():
+    all = cr.execute("""
+        SELECT first_name, last_name, debut_year
+        FROM player
+        WHERE points_scored = (SELECT MAX(points_scored) FROM player)
+        ;
+    """).fetchall()
 
-        for client in all:
-            print(f"{client[0]} {client[1]}, {client[2]}")
-        db.close()
+    for client in all:
+        print(f"{client[0]} {client[1]}, {client[2]}")
+    db.close()
 
-    def fifty_tests():
-        all = cr.execute("""
-            SELECT first_name, last_name
-            FROM player
-            WHERE debut_year > 2010
-                AND test_caps > 50
-            ;
-        """).fetchall()
+def fifty_tests():
+    all = cr.execute("""
+        SELECT first_name, last_name
+        FROM player
+        WHERE debut_year > 2010
+            AND test_caps > 50
+        ;
+    """).fetchall()
 
-        for client in all:
-            print(f"{client[0]} {client[1]}")
-        db.close()
+    for client in all:
+        print(f"{client[0]} {client[1]}")
+    db.close()
 
